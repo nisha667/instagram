e04377f4061be3a57fd7e298cdd24da8513cab3a
nisha667 <42538319+nisha667@users.noreply.github.com>
2019-01-24 08:44:16 +0530

    Add files via upload

---
+++ project3frontend/WebContent/services/chatservice.js
@@ -0,0 +1,8 @@
+rs>
+  <descriptors xmi:id="_nXGTQx65EemxT8oP4iDHtg" elementId="org.eclipse.debug.ui.ModuleView" label="Modules" iconURI="platform:/plugin/org.eclipse.debug.ui/icons/full/eview16/module_view.png" tooltip="" allowMultiple="true" category="Debug" closeable="true" contributionURI="bundleclass://org.eclipse.ui.workbench/org.eclipse.ui.internal.e4.compatibility.CompatibilityView">
+    <persistedState key="originalCompatibilityViewClass" value="org.eclipse.debug.internal.ui.views.modules.ModulesView"/>
+    <persistedState key="originalCompatibilityViewBundle" value="org.eclipse.debug.ui"/>
+    <tags>View</tags>
+    <tags>categoryTag:Debug</tags>
+  </descriptors>
+  <descriptors xmi:id="_nXQEQB65EemxT8oP4iDHtg" elementId="org.eclipse.debug.ui.MemoryView" label="Memory" iconURI="platform:/plugin/org.eclipse.debug.ui/icons/full/eview
---
+++ project3frontend/WebContent/services/userservice.js
@@ -0,0 +1,32 @@
+/**
+ * UserService
+ */
+app.factory('UserService',function($http){
+	var userService={}
+	var BASE_URL="http://localhost:8060/project2middleware"
+	
+	userService.registerUser=function(user){//user <- usercontroller  <-registrationform.html
+	 return	$http.post(BASE_URL+ "/register",user)
+	}
+	
+	userService.login=function(user){
+		return $http.put(BASE_URL+"/login",user)
+	}
+	
+	userService.getProtectedResource=function(){
+		return $http.get(BASE_URL + "/protectedresource")
+	}
+	
+	userService.logout=function(user){
+		return $http.put(BASE_URL + "/logout",user)
+	}
+
+	userService.getUser=function(){
+		return $http.get(BASE_URL + "/getuser")
+	}
+	userService.update=function(user){
+		return $http.put(BASE_URL + "/updateprofile",user)
+	}
+	return userService;
+})
+
